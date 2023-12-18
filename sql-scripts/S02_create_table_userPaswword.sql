1424a927b1d0dab484622c3ae753183ad2c1efac
van wsl2 <pvphat25@gmail.com>
2023-12-18 18:54:27 -0500

    add email to UserPassword as username

--- sql-scripts/S02_create_table_userPaswword.sql
+++ sql-scripts/S02_create_table_userPaswword.sql
@@ -1,6 +1,6 @@
 CREATE TABLE UserPassword (
     ID INT PRIMARY KEY,
-    UserID INT,
-    PasswordHash VARCHAR(255),
+    Email VARCHAR(255),
+    Password VARCHAR(255),
     FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
