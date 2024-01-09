0937b654457e6e532f54851be49689e63d139af4
van wsl2 <pvphat25@gmail.com>
2024-01-09 16:10:32 -0500

    modify product table

--- sql-scripts/S04_create_table_product.sql
+++ sql-scripts/S04_create_table_product.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Product (
     ProductID INT PRIMARY KEY AUTO_INCREMENT,
     Name VARCHAR(255),
-    Description VARCHAR(255),
+    Description VARCHAR(255 ),
     Price DECIMAL(10, 2),
     CategoryID INT,
     FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID)
